012fc5c99f9b262dcb3b1731a324058f0751d2b6
Briantoroflh <briantoroflh@gmail.com>
2025-08-31 11:39:08 +0700

    fix: name url route

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BillController;
 use App\Http\Controllers\Api\CurrentBillController;
-use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\DetailBillController;
-use App\Http\Controllers\Api\ManageOfficerController;
-use App\Http\Controllers\Api\ManageSppController;
-use App\Http\Controllers\Api\ManageStudentController;
 use App\Http\Controllers\Api\OfficerController;
-use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\StudentController;
-use App\Models\CurrentBill;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Officer Authentication
@@ -30,17 +23,17 @@ Route::post('/student/reset-password', [AuthController::class, 'studentResetPass
 
 Route::middleware(['auth:sanctum', 'ability:access-token'])->group(function () {
 
-    Route::get('/current-bill/{id}', [CurrentBillController::class, 'getCurrentBillByUuidStudent']);
-    Route::get('/detail-bill', [DetailBillController::class. '']);
+    Route::get('/student/current-bill/{uuid}', [CurrentBillController::class, 'getCurrentBillByUuidStudent']);
+    Route::get('/student/detail-bill/{uuid}', [DetailBillController::class. '']);
 
     Route::middleware('role:admin')->group(function () {
-        Route::apiResource('/student', StudentController::class);
-        Route::apiResource('/role', RoleController::class);
-        Route::apiResource('/officer', OfficerController::class);
+        Route::apiResource('/dashboard/student', StudentController::class);
+        Route::apiResource('/dashboard/role', RoleController::class);
+        Route::apiResource('/dashboard/officer', OfficerController::class);
     });
 
     Route::middleware('role:tu')->group(function () {
-        Route::apiResource('/bill', BillController::class);
-        Route::apiResource('/detail-bill', DetailBillController::class);
+        Route::apiResource('/dashboard/manage/bill', BillController::class);
+        Route::apiResource('/dashboard/manage/detail-bill', DetailBillController::class);
     });
 });
